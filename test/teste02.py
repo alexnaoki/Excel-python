843f04503ca6065cd4c45a43f2ae132c86ebb405
alexnaoki <alexkobayashi10@gmail.com>
2021-09-05 12:48:11 -0400

    First test commit

---
+++ test/teste02.py
@@ -0,0 +1,38 @@
+import xlwings as xw
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import sympy.physics.units as u
+
+
+
+@xw.sub  # only required if you want to import it or run it via UDF Server
+def main():
+    wb = xw.Book.caller()
+    wb.sheets[0].range("A1").value = "Hello xlwings!"
+
+
+@xw.func
+def hello(name):
+    return "hello {0}22222".format(name)
+
+@xw.func
+def teste(x):
+    return f"testeando {x}"
+
+@xw.func
+@xw.ret(expand='table')
+@xw.arg('diametro', doc='Diametro em mm')
+def area_aco(diametro):
+    '''
+    diametro em mm
+    area_tranversal em cm2
+    '''
+    diametro = diametro*u.mm
+    area_tranversal = math.pi*diametro**2/4
+    area_tranversal = u.convert_to(area_tranversal,u.cm**2)
+    return [area_tranversal.args[0], str(area_tranversal.args[1])]
+
+if __name__ == "__main__":
+    xw.books.active.set_mock_caller()
+    main()
