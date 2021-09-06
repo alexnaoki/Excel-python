c0eb0ae50839204f10ef0deb4e29f30155be0d11
alexnaoki <alexkobayashi10@gmail.com>
2021-09-05 21:06:15 -0400

    Update tests

--- test/teste02.py
+++ test/teste02.py
@@ -31,7 +31,19 @@ def area_aco(diametro):
     diametro = diametro*u.mm
     area_tranversal = math.pi*diametro**2/4
     area_tranversal = u.convert_to(area_tranversal,u.cm**2)
-    return [area_tranversal.args[0], str(area_tranversal.args[1])]
+    # return [area_tranversal.args[0], str(area_tranversal.args[1])]
+    return area_tranversal
+
+
+@xw.func
+def aco_opcoes(area_necessaria):
+    bitolas = [5, 6.3, 8, 10, 12.5, 16, 20, 25]
+
+    area_necessaria = area_necessaria*u.cm**2
+
+
+    return [[a]+[math.ceil(area_necessaria/area_aco(diametro=a))] for a in bitolas]
+
 
 if __name__ == "__main__":
     xw.books.active.set_mock_caller()
