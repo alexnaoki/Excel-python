17d8c16b98aef82e451bbca6dbbbca5913eb9477
alexnaoki <alexkobayashi10@gmail.com>
2021-12-25 22:52:22 -0400

    Refactor variables and minor improvement of code

--- main/optimize_steel_cut/optimize_steel_cut.py
+++ main/optimize_steel_cut/optimize_steel_cut.py
@@ -6,58 +6,71 @@ def main():
     wb = xw.Book.caller()
     sheet = wb.sheets[0]
 
-    codigo_range_i = xw.Range('A3')
-    codigo_range_e = codigo_range_i.end('down')
-
-    # print(codigo_range_e.row)
+    print('Reading Input')
+    inputTable_Range = xw.Range('A3')
+    inputTable_Range_end = inputTable_Range.end('down')
     
-    df = codigo_range_i.expand().options(pd.DataFrame).value.reset_index()
-    # print(df)
-    # print(df['Bitola (mm)'].unique())
-    bins_used = 2
+    print('Creating DataFrame')
+    df_inputTable = inputTable_Range.expand().options(pd.DataFrame).value.reset_index()
 
-    results_row = codigo_range_e.row+bins_used
-    for bitola in df['Bitola (mm)'].unique():
+    results_row = inputTable_Range_end.row+3
+
+    results_Range = xw.Range((results_row, inputTable_Range.column))
+    print('Clearing previous result...')
+    clear_range_e = xw.Range((results_row, inputTable_Range.column)).end('down')
+    clear_range_e = clear_range_e.end('right')
+    # clear_range_e.color = (255,0,0)
+    xw.Range(results_Range, clear_range_e).clear()
+    print('Done Clearing')
+    
+    print('Optimizing:')
+    for bitola in df_inputTable['Bitola (mm)'].unique():
         print(f'BITOLA: {bitola} mm')
-        xw.Range((results_row-1, codigo_range_e.column)).value = f'BITOLA: {bitola} mm'
-        n_list = df.loc[df['Bitola (mm)']==bitola]
+        xw.Range((results_row-1, inputTable_Range_end.column)).value = f'BITOLA: {bitola} mm'
+        xw.Range((results_row, inputTable_Range_end.column)).value = [['Used', 'Wasted']]
+        results_row += 1
+
+        df_bitola = df_inputTable.loc[df_inputTable['Bitola (mm)']==bitola]
         
-        weights = []
-
-        for i, n in n_list.iterrows():
-            print(f'ROWS: {results_row}')
+        # List all segments for a particular diameter
+        segments = []
+        for i, n in df_bitola.iterrows():
+            # print(f'ROWS: {results_row}')          
+            segments += [float(n['Comprimento (cm)'])]*int(n['Quantidade'])
             
+        data = {}
+        data['segments'] = segments
+        data['items'] = list(range(len(segments)))
+        data['bins'] = data['items']
+        data['bin_capacity'] = 1200
 
-            weights += [float(n['Comprimento (mm)'])]*int(n['Quantidade'])
-            
-            data = {}
-            data['weights'] = weights
-            data['items'] = list(range(len(weights)))
-            data['bins'] = data['items']
-            data['bin_capacity'] = 1200
+        # Optimize function
+        run = optimize_steel_cut(data=data)
 
-            run = optimize_steel_cut(data=data)
-            print(run)
-
-
-            print(run[3])
+        # Display results
         for i, r in enumerate(run[3]):
+            # List of segments for 1 1200cm rebar
+            xw.Range((results_row, inputTable_Range_end.column+2)).value = run[3][i]
             
-            xw.Range((results_row, codigo_range_e.column)).value = run[3][i]
-            # xw.Range((results_row+i, codigo_range_e.column)).value = run[2][i]
-            # bins_used = run[0]
+            # Used and Wasted 1200cm rebar
+            xw.Range((results_row, inputTable_Range_end.column)).value = run[2][i]      
+            xw.Range((results_row, inputTable_Range_end.column+1)).value = 1200-run[2][i]
 
-            # results_row += bins_used
+            xw.Range((results_row, inputTable_Range_end.column)).color = (0,255,0)      # Green
+            xw.Range((results_row, inputTable_Range_end.column+1)).color = (255,0,0)    # Red
+
             results_row += 1
 
         results_row += 1
+        print('Done')
 
 
     
 def optimize_steel_cut(data):
-    print('OPTIMIZING')
-    print(data)
+    # print('OPTIMIZING')
+    # print(data)
     solver = pywraplp.Solver.CreateSolver('SCIP')
+    
     # Variables
     # x[i, j] = 1 if item i is packed in bin j.
     x = {}
@@ -78,7 +91,7 @@ def optimize_steel_cut(data):
     # The amount packed in each bin cannot exceed its capacity.
     for j in data['bins']:
         solver.Add(
-            sum(x[(i, j)] * data['weights'][i] for i in data['items']) <= y[j] *
+            sum(x[(i, j)] * data['segments'][i] for i in data['items']) <= y[j] *
             data['bin_capacity'])
 
     # Objective: minimize the number of bins used.
@@ -101,26 +114,24 @@ def optimize_steel_cut(data):
                 for i in data['items']:
                     if x[i, j].solution_value() > 0:
                         bin_items.append(i)
-                        bin_weight += data['weights'][i]
-                        bin_pieces.append(data['weights'][i])
+                        bin_weight += data['segments'][i]
+                        bin_pieces.append(data['segments'][i])
                 if bin_weight > 0:
                     num_bins += 1
                     all_bin_items.append(bin_items)
                     all_bin_weight.append(bin_weight)
                     all_bin_pieces.append(bin_pieces)
-                    print('Bin number', j)
-                    print('  Items packed:', bin_items)
-                    print('  Total weight:', bin_weight)
-                    print()
-        print()
-        print('Number of bins used:', num_bins)
-        print('Time = ', solver.WallTime(), ' milliseconds')
+        #             print('Bin number', j)
+        #             print('  Items packed:', bin_items)
+        #             print('  Total weight:', bin_weight)
+        #             print()
+        # print()
+        # print('Number of bins used:', num_bins)
+        # print('Time = ', solver.WallTime(), ' milliseconds')
         return (num_bins, all_bin_items, all_bin_weight, all_bin_pieces)
     else:
         print('The problem does not have an optimal solution.')
-
-
-
+        
 if __name__ == "__main__":
     xw.Book("optimize_steel_cut.xlsm").set_mock_caller()
     main()
